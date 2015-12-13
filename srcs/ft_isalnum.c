7708f024bf17d257988ed9942a7c5fa2e5acd2bc
tdieumeg <tdieumeg@student.42.fr>
2015-12-13 12:59:25 +0100

    update

--- srcs/ft_isalnum.c
+++ srcs/ft_isalnum.c
@@ -14,10 +14,5 @@
 
 int			ft_isalnum(int c)
 {
-	if ((c >= '0' && c <= '9')
-		|| (c >= 'a' && c <= 'z')
-		|| (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return (ft_isdigit(c) || ft_isalpha(c));
 }
